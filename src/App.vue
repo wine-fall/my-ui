33c2dad7deed80561d3a8c10f661ab85d33aff99
fall-wine <wine@mail.bnu.edu.cn>
2020-05-02 21:24:07 +0800

    button部分

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="row">
+        <g-button type="success">success</g-button>
+        <g-button type="primary">primary</g-button>
+        <g-button type="info">info</g-button>
+        <g-button type="warning">warning</g-button>
+        <g-button type="danger">danger</g-button>
+        <g-button>default</g-button>
+      </div>
+      <div class="row">
+        <g-button plain type="success">success</g-button>
+        <g-button circle type="primary">primary</g-button>
+        <g-button round type="info">info</g-button>
+        <g-button type="warning">warning</g-button>
+        <g-button type="danger">danger</g-button>
+        <g-button>default</g-button>
+      </div>
+      <div class="row">
+        <g-button @click="changeIcon" icon="el-icon-check" type="success">success</g-button>
+        <g-button icon="el-icon-delete" type="primary">primary</g-button>
+        <g-button icon="el-icon-edit" type="info">info</g-button>
+        <g-button icon="el-icon-check" type="warning"></g-button>
+        <g-button icon="el-icon-check" type="danger"></g-button>
+        <g-button icon="el-icon-edit"></g-button>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    methods:{
+        changeIcon(){
+            console.log(1);
+        }
+    }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row{
+    display: flex;
+    margin-bottom: 15px;
+    justify-content: space-between;
+    width: 600px;    
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Gbutton from './components/mybutton.vue'
+import './assets/fonts/font.scss'
 Vue.config.productionTip = false
-
+Vue.component(Gbutton.name, Gbutton)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
